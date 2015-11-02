ee2e4d8ab7c7596a20e374f9a7e71cef4e37c886
Ben Foxall <ben@whiteoctober.co.uk>
2015-11-02 21:07:44 +0000

    show all the minutes in the last hour

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+var moment = require('moment');
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+  var m = moment().startOf('minute');
+
+  // minute since epoch in hex
+  var etag = Math.floor(+m / 60000).toString(32);
+  res.set('etag', etag); // I don't think this is supposed to work
+
+  for(var times = [], i = 0; i < 60; i++){
+    times.push(m.format('LTS'));
+    m.subtract(1, 'minutes');
+  }
+
+  // yup
+  res.send('<ul>\n' +
+    times.map(function(t){
+      return '\t<li>' + t + '</li>\n';
+    }).join('') +
+  '</ul>\n');
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
