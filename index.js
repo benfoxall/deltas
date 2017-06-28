78afa8a8843c2dd772b6e178d2b6bb6c2d797ba1
Ben Foxall <benfoxall@gmail.com>
2017-06-28 08:45:01 +0100

    Simplify backend by making body function of etag

--- index.js
+++ index.js
@@ -1,62 +1,40 @@
 var diff = require('diff');
-var Redis = require('ioredis');
-var redis = new Redis(process.env.REDISTOGO_URL);
-var cons = require('consolidate');
 var moment = require('moment');
 var express = require('express');
 var app = express();
 
-app.engine('html', cons.hogan);
-
-app.set('views', './public');
-app.set('view engine', 'html');
-
 app.get('/feed', function(req, res) {
 
-  var m = moment();
-  var s = Math.floor(m.seconds() / 10) * 10;
-  m.milliseconds(0).seconds(s);
+  const at = nearest(10000, Date.now())
+  const body = responseBody(at)
+  const etag = at.toString(36)
 
-  // 10s since epoch in hex
-  var etag = Math.floor(+m / 10000).toString(32);
-  res.set('etag', etag); // I don't think this is supposed to work
-
-  for (var times = [], i = 0; i < 60; i++) {
-    times.push('<li>' + m.format('LTS') + '</li> \n');
-    m.subtract(10, 'seconds');
-  }
-
-  var htm = '<ul>\n' + times.join('') + '</ul>';
-
-  //start diff stuff
-  redis.setex(etag, 60000, htm);
+  res.set('etag', etag)
 
   if (req.headers['a-im'] == 'x-js-diff' && req.headers['if-none-match']) {
     // attempt to return a diff
-    console.log('ATTEMPT IM');
+    console.log('ATTEMPT IM RESPONSE')
 
-    if (req.headers['if-none-match'] == etag) {
-      console.log('same etag, resume');
-      res.send(304);
-    } else {
+    const lastETag = req.headers['if-none-match']
 
-      console.log('checking for %s -> %s', req.headers['if-none-match'], etag);
-      redis.get(req.headers['if-none-match'])
-        .then(function(data) {
-          var d = diff.createTwoFilesPatch('feed', 'feed', data, htm, '', '', {context: 1});
-          console.log(d);
-
-          // TODO res.send(226, d);
-        });
-
-      // for now just send
-      res.send(htm);
-
+    if (lastETag === etag) {
+      console.log(`304: unchanged from ${etag}`)
+      return res.send(304)
     }
 
-  } else {
-    res.send(htm);
+    const lastBody = responseBodyForETag(lastETag)
+
+    if(lastBody) {
+      console.log(`226: from ${lastETag} to ${etag}`)
+
+      const patch = diff.createTwoFilesPatch('feed', 'feed', lastBody, body, '', '', {context: 0})
+      console.log(patch)
+
+      return res.send(226, patch)
+    }
   }
+
+  return res.send(responseBody(at))
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -65,5 +43,29 @@ var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('App listening at http://%s:%s', host, port);
+  console.log('listening at http://%s:%s', host, port);
 });
+
+
+function nearest(to, value) {
+  return Math.round(value / to) * to
+}
+
+// generate a response (an svg clock would be a cool example)
+function responseBody(time) {
+  var m = moment(time)
+
+  for (var times = [], i = 0; i < 60; i++) {
+    times.push('<li>' + m.format('LTS') + '</li> \n')
+    m.subtract(10, 'seconds')
+  }
+
+  return '<ul style="font-family:sans-serif">\n' + times.join('') + '</ul>'
+}
+
+// This is the part that's challenging.
+// We'd need to be able to store/recreate any version of the content
+function responseBodyForETag(tag) {
+  const time = parseInt(tag, 36)
+  return isFinite(time) && responseBody(time)
+}
